aba39d5216c7b29c4b896759b8dc3cec2697f027
Andreirl032 <andrei.ramos.lopes@gmail.com>
2024-03-03 00:08:00 -0300

    ajustada tela inicial, adicionado toggle dark mode

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,20 @@
 const Header = () => {
   return (
-    <header className="flex items-center justify-around mx-3 mt-3 rounded-lg border-gray-600 border-[1px] h-16 w-auto flex-row text-white shadow-xl">
+    <header className="flex items-center justify-between mx-3 mt-3 rounded-lg border-gray-600 border-[1px] h-16 w-auto flex-row text-white shadow-xl">
+      <div className="flex flex-row justify-around min-w-[60%]">
         <h2>Início</h2>
         <h2>Tecnologias</h2>
         <h2>Projetos</h2>
         <h2>Contato</h2>
+      </div>
+      <div className="mr-10 inline-flex items-center">
+  <span className="me-3 text-sm font-medium text-[#e6e6e6] dark:text-gray-300">Modo claro</span>
+      <label className="inline-flex items-center cursor-pointer">
+  <input type="checkbox" value="" className="sr-only peer"/>
+  <div className="transition relative w-[3.45rem] h-7 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[0.19rem] after:start-[6px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-[1.4rem] after:w-[1.4rem] after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+</label>
+  <span className="ms-3 text-sm font-medium text-[#e6e6e6] dark:text-gray-300">Modo escuro</span>
+</div>
     </header>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,50 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Header from "./components/Header";
-import myself from "../../public/assets/eu.png"
+import myself from "../../public/assets/eu.png";
 import { useEffect } from "react";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Home() {
+  //Inicialização do AOS
   useEffect(() => {
     AOS.init({
-         duration: 600,
-         once: false,
-       })
- }, [])
+      duration: 600,
+      once: false,
+    });
+  }, []);
+
   return (
     <>
       <body>
-        <Header/>
+        {/* Header da página */}
+        <Header />
+
         <main className="mt-16 overflow-x-hidden">
-          <div className="flex flex-row items-center justify-between">
-          <div className="ml-16" data-aos="fade-right">
-          <h3 className="text-xl">Bem vindo!</h3>
-          <h1 className="font-bold text-4xl">Eu sou Andrei Ramos Lopes</h1>
-          <h2 className="text-2xl">Desenvolvedor Front-end</h2>
-          </div>
-          <div className="mr-16" data-aos="fade-left">
-          <Image width={444} height={444} src={myself} alt="myself" className="rounded-3xl"></Image>
-          </div>
+          {/* Container da página inicial */}
+          <div className="flex w-auto flex-row items-center justify-between">
+            {/* Texto de descrição */}
+            <div className="ml-24 w-full" data-aos="fade-right">
+              <h3 className="text-xl">Bem vindo!</h3>
+              <h1 className="font-bold text-4xl">Eu sou Andrei Ramos Lopes</h1>
+              <h2 className="text-2xl">Desenvolvedor Front-end</h2>
+              
+              <h2 className="text-2xl mt-10 break-normal">Sou estudante de programação pela Universidade Federal do Maranhão (UFMA) e programador Front-end.</h2>
+            </div>
+
+            {/* Foto de mim mesmo */}
+            <div className="ml-auto mr-24 w-full" data-aos="fade-left">
+              <Image
+                width={444}
+                height={444}
+                src={myself}
+                alt="myself"
+                className="rounded-3xl ml-auto"
+              />
+            </div>
           </div>
         </main>
       </body>
