761cce501720de0a65120d4dd07950cf661fb9f8
Andreirl032 <andrei.ramos.lopes@gmail.com>
2025-06-20 23:48:49 -0300

    adicionadas imagens (necessitam ajuste)

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,13 @@
 "use client"
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 
-const Header = () => {
+interface Reference{
+ref:MutableRefObject<null | HTMLDivElement>
+}
+
+const Header = ({ref}: Reference) => {
   const [mounted, setMounted] = useState(false)
 
   const { theme, setTheme } = useTheme();
@@ -12,18 +16,20 @@ const Header = () => {
     console.log(theme);
   },[theme])
   return (
-    <header className="flex items-center justify-between mx-3 mt-3 rounded-lg border-black dark:border-gray-600 border-[1px] h-16 w-auto flex-row text-black dark:text-white shadow-xl">
+    <header className="flex sticky items-center justify-between mx-3 mt-3 rounded-lg border-black dark:border-gray-600 border-[1px] h-16 w-auto flex-row text-black dark:text-white shadow-xl">
       <div className="flex flex-row justify-around min-w-[60%]">
-        <h2>Início</h2>
+        <button onClick={()=>ref.current?.scrollIntoView({behavior:'smooth'})}>
+          <h2>Início</h2>
+          </button>
         <h2>Tecnologias</h2>
         <h2>Projetos</h2>
         <h2>Contato</h2>
       </div>
        {!mounted ? null :
       <div className="mr-10 inline-flex items-center">
-  <span className="me-3 text-sm font-medium text-[#e6e6e6] dark:text-gray-300">Modo claro</span>
+  <span className="me-3 text-sm font-medium text-black dark:text-gray-300">Modo claro</span>
       <label className="inline-flex items-center cursor-pointer">
-  <input type="checkbox" value="" className="sr-only peer"
+  <input type="checkbox" value="" checked={theme==="dark" ? true : false} className="sr-only peer"
         onChange={() => {
           if (theme === 'dark') {
             return setTheme('light')
@@ -31,9 +37,9 @@ const Header = () => {
           return setTheme('dark')
         }}
         />
-  <div className="transition relative w-[3.45rem] h-7 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[0.19rem] after:start-[6px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-[1.4rem] after:w-[1.4rem] after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+  <div className="transition relative w-[3.45rem] h-7 bg-gray-500 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[0.19rem] after:start-[6px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-[1.4rem] after:w-[1.4rem] after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
 </label>
-  <span className="ms-3 text-sm font-medium text-[#e6e6e6] dark:text-gray-300">Modo escuro</span>
+  <span className="ms-3 text-sm font-medium text-black dark:text-gray-300">Modo escuro</span>
 </div>}
     </header>
   );
