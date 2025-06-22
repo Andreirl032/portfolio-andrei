0432e533cf495dd96695001633658598381848c5
Andreirl032 <andrei.ramos.lopes@gmail.com>
2025-06-22 12:30:55 -0300

    ajustado header e postas imagens

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -16,18 +16,18 @@ const Header = ({ref}: Reference) => {
     console.log(theme);
   },[theme])
   return (
-    <header className="flex sticky items-center justify-between mx-3 mt-3 rounded-lg border-black dark:border-gray-600 border-[1px] h-16 w-auto flex-row text-black dark:text-white shadow-xl">
-      <div className="flex flex-row justify-around min-w-[60%]">
+    <header className="flex left-3 top-3 z-10 sticky bg-[#e6e6e6] dark:bg-[#122131] items-center justify-between mx-3 mt-3 rounded-lg border-black dark:border-gray-600 border-[2px] h-20 w-auto flex-row text-black dark:text-white shadow-xl">
+      <div className="flex flex-row justify-around min-w-[70%]">
         <button onClick={()=>ref.current?.scrollIntoView({behavior:'smooth'})}>
-          <h2>Início</h2>
+          <h2 className="text-xl">Início</h2>
           </button>
-        <h2>Tecnologias</h2>
-        <h2>Projetos</h2>
-        <h2>Contato</h2>
+        <h2 className="text-xl">Tecnologias</h2>
+        <h2 className="text-xl">Projetos</h2>
+        <h2 className="text-xl">Contato</h2>
       </div>
        {!mounted ? null :
       <div className="mr-10 inline-flex items-center">
-  <span className="me-3 text-sm font-medium text-black dark:text-gray-300">Modo claro</span>
+  <span className="me-3 text-md font-medium text-black dark:text-gray-300">Modo claro</span>
       <label className="inline-flex items-center cursor-pointer">
   <input type="checkbox" value="" checked={theme==="dark" ? true : false} className="sr-only peer"
         onChange={() => {
@@ -39,7 +39,7 @@ const Header = ({ref}: Reference) => {
         />
   <div className="transition relative w-[3.45rem] h-7 bg-gray-500 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[0.19rem] after:start-[6px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-[1.4rem] after:w-[1.4rem] after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
 </label>
-  <span className="ms-3 text-sm font-medium text-black dark:text-gray-300">Modo escuro</span>
+  <span className="ms-3 text-md font-medium text-black dark:text-gray-300">Modo escuro</span>
 </div>}
     </header>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,26 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import myself from "../../public/assets/eu.png";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+import html from "../../public/assets/logos/html.png";
+import css from "../../public/assets/logos/css.png";
+import js from "../../public/assets/logos/js.png";
+import ts from "../../public/assets/logos/ts.png";
+import react from "../../public/assets/logos/react.png";
+import next from "../../public/assets/logos/next.png";
+import python from "../../public/assets/logos/python.png";
+import sql from "../../public/assets/logos/sql.png";
+import firebase from "../../public/assets/logos/firebase.png";
+import mongodb from "../../public/assets/logos/mongodb.png";
+import tailwind from "../../public/assets/logos/tailwind.png";
+import native from "../../public/assets/logos/html.png";
+
 export default function Home() {
+  const scrollRef = useRef<null | HTMLDivElement>(null);
   //Inicialização do AOS
   useEffect(() => {
     AOS.init({
@@ -19,35 +33,172 @@ export default function Home() {
 
   return (
     <>
-      <body>
-        {/* Header da página */}
-        <Header />
+      {/* Header da página */}
+      <Header ref={scrollRef} />
 
-        <main className="mt-16 overflow-x-hidden">
-          {/* Container da página inicial */}
-          <div className="flex w-auto flex-row items-center justify-between">
-            {/* Texto de descrição */}
-            <div className="ml-24 w-full" data-aos="fade-right">
-              <h3 className="text-xl">Bem vindo!</h3>
-              <h1 className="font-bold text-4xl">Eu sou Andrei Ramos Lopes</h1>
-              <h2 className="text-2xl">Desenvolvedor Front-end</h2>
-              
-              <h2 className="text-2xl mt-10 break-normal">Sou estudante de programação pela Universidade Federal do Maranhão (UFMA) e programador Front-end.</h2>
-            </div>
+      <main className="mt-16 overflow-x-hidden">
+        {/* Container da página inicial */}
+        <section
+          className="flex w-auto flex-row items-center justify-between"
+          ref={scrollRef}
+        >
+          {/* Texto de descrição */}
+          <div className="ml-24 w-full" data-aos="fade-right">
+            <h3 className="text-xl">Bem vindo!</h3>
+            <h1 className="font-bold text-4xl">Eu sou Andrei Ramos Lopes</h1>
+            <h2 className="text-2xl">Desenvolvedor front-end</h2>
 
-            {/* Foto de mim mesmo */}
-            <div className="ml-auto mr-24 w-full" data-aos="fade-left">
-              <Image
-                width={444}
-                height={444}
-                src={myself}
-                alt="myself"
-                className="rounded-3xl ml-auto"
-              />
+            <h2 className="text-2xl mt-10 break-normal">
+              Sou estudante de programação pela Universidade Federal do Maranhão
+              (UFMA) e programador Front-end.
+            </h2>
+          </div>
+
+          {/* Foto de mim mesmo */}
+          <div className="ml-auto mr-24 w-full" data-aos="fade-left">
+            <Image
+              width={444}
+              height={444}
+              src={myself}
+              alt="myself"
+              className="rounded-3xl ml-auto"
+            />
+          </div>
+        </section>
+        <section
+          className="flex w-auto items-center justify-center mt-28"
+          ref={scrollRef}
+        >
+          <div className="flex flex-col gap-8 items-center">
+            <h1 className="font-bold text-4xl">Tecnologias</h1>
+            <h2 className="w-[50%] text-2xl text-center">
+              Tenho conhecimentos principalmente em desenvolvimento web e
+              mobile, e ao longo do tempo aprendi acerca de diversas
+              tecnologias, sendo as principais:
+            </h2>
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-row gap-10 justify-center items-center">
+                <div className="max-w-18 max-h-18">
+                  <Image
+                    height={100}
+                    width={100}
+                    objectFit="contain"
+                    src={html}
+                    alt="html"
+                  />
+                </div>
+                <div className="max-w-18 max-h-18">
+                  <Image
+                    height={100}
+                    width={100}
+                    objectFit="contain"
+                    src={css}
+                    alt="css"
+                  />
+                </div>
+                <div className="max-w-18 max-h-18">
+                  <Image
+                    height={100}
+                    width={100}
+                    objectFit="contain"
+                    src={js}
+                    alt="js"
+                  />
+                </div>
+                <div className="max-w-18 max-h-18">
+                  <Image
+                    height={100}
+                    width={100}
+                    objectFit="contain"
+                    src={ts}
+                    alt="ts"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-row gap-10">
+                <div className="max-w-18 max-h-18">
+                  <Image
+                    height={100}
+                    width={100}
+                    objectFit="contain"
+                    src={react}
+                    alt="react"
+                  />
+                </div>
+                <div className="max-w-18 max-h-18">
+                  <Image
+                    height={100}
+                    width={100}
+                    objectFit="contain"
+                    src={native}
+                    alt="native"
+                  />
+                </div>
+                <div className="max-w-18 max-h-18">
+                  <Image
+                    height={100}
+                    width={100}
+                    objectFit="contain"
+                    src={next}
+                    alt="next"
+                  />
+                </div>
+                <div className="max-w-18 max-h-18">
+                  <Image
+                    height={100}
+                    width={100}
+                    objectFit="contain"
+                    src={tailwind}
+                    alt="tailwind"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-row gap-10">
+                <div className="max-w-18 max-h-18">
+                  <Image
+                    height={100}
+                    width={100}
+                    objectFit="contain"
+                    src={python}
+                    alt="python"
+                  />
+                </div>
+                <div className="max-w-18 max-h-18">
+                  <Image
+                    height={100}
+                    width={100}
+                    objectFit="contain"
+                    src={sql}
+                    alt="sql"
+                  />
+                </div>
+                <div className="max-w-18 max-h-18">
+                  <Image
+                    height={100}
+                    width={100}
+                    objectFit="contain"
+                    src={firebase}
+                    alt="firebase"
+                  />
+                </div>
+                <div className="max-w-18 max-h-18">
+                  <Image
+                    height={100}
+                    width={100}
+                    objectFit="contain"
+                    src={mongodb}
+                    alt="mongodb"
+                  />
+                </div>
+              </div>
             </div>
           </div>
-        </main>
-      </body>
+        </section>
+        <section
+          className="flex w-auto items-center justify-center mt-28"
+          ref={scrollRef}
+        ></section>
+      </main>
     </>
   );
 }
